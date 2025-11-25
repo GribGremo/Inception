d04894f7e9a67ea270d4f2905dac28d29c829413
sylabbe <sylabbe@Inception-Debian-Trixie.myguest.virtualbox.org>
2025-11-25 16:58:34 +0100

    BAPASQUI A FUTTU LA MERDE

--- srcs/mariadb/db_setup.sh
+++ srcs/mariadb/db_setup.sh
@@ -4,20 +4,24 @@
 set -e
 
 # Variables attendues via env_file (.env), return an error if missing or not init
-: "${MYSQL_DB_NAME:?Need MYSQL_DB_NAME}"
-: "${MYSQL_USER_NAME:?Need MYSQL_USER_NAME}"
-: "${MYSQL_USER_PASSWORD:?Need MYSQL_USER_PASSWORD}"
-: "${MYSQL_ROOT_PASSWORD:-root}"   # optionnel, default si absent
 
+#: "${MYSQL_DB_NAME:?Need MYSQL_DB_NAME}"
+#: "${MYSQL_USER_NAME:?Need MYSQL_USER_NAME}"
+#: "${MYSQL_USER_PASSWORD:?Need MYSQL_USER_PASSWORD}"
+#: "${MYSQL_ROOT_PASSWORD:-root}"   # optionnel, default si absent
+
+env
 # only run if DB not yet initialized
-if [ ! -d "/var/lib/mysql/${MYSQL_DB_NAME}" ]; then
+#if [ ! -d "/var/lib/mysql/${MYSQL_DB_NAME}" ]; then
     mysqld_safe --skip-networking &
     sleep 5  # attendre que le serveur démarre
-    mysql -e "CREATE DATABASE ${MYSQL_DB_NAME};"
-    mysql -e "CREATE USER '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER_NAME}'@'%';"
-    mysql -e "FLUSH PRIVILEGES;"
-    killall mysqld
-fi
+    mysql -u root -e "CREATE DATABASE ${MYSQL_DB_NAME};"
+    mysql -u root -e "CREATE USER '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER_NAME}'@'%';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+	
+#fi
 
-exec mysqld_safe
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+exec mysqld
