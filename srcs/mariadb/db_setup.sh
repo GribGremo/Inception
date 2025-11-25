d58fdcf32f1cfc4707afdf05055199e54e9d8567
GribGremo <labbesylvainpro@gmail.com>
2025-11-25 09:36:16 +0100

    d

--- srcs/mariadb/db_setup.sh
+++ srcs/mariadb/db_setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#Mechanism to prevent script to continue in case something return an error during script
+set -e
+
+# Variables attendues via env_file (.env), return an error if missing or not init
+: "${MYSQL_DB_NAME:?Need MYSQL_DB_NAME}"
+: "${MYSQL_USER_NAME:?Need MYSQL_USER_NAME}"
+: "${MYSQL_USER_PASSWORD:?Need MYSQL_USER_PASSWORD}"
+: "${MYSQL_ROOT_PASSWORD:-root}"   # optionnel, default si absent
+
 # only run if DB not yet initialized
 if [ ! -d "/var/lib/mysql/${MYSQL_DB_NAME}" ]; then
     mysqld_safe --skip-networking &
