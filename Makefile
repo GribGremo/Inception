8c4a6505c3960ab4976bf484c41efa37a5541474
sylabbe <sylabbe@Inception-Debian-Trixie.myguest.virtualbox.org>
2025-12-09 12:53:38 +0100

    b reu

--- Makefile
+++ Makefile
@@ -11,9 +11,18 @@ NC=\033[0m
 #                                   COMMANDS                                   #
 # **************************************************************************** #
 
-DOCKER_COMPOSE = docker compose -f ./srcs/docker-compose.yml
-VOLUMES = /home/sylabbe/data/wordpress /home/sylabbe/data/mariadb
-SETUP_SCRIPT = ./srcs/setup.sh
+SRC_PATH = ./srcs
+VLM_PATH = /home/sylabbe/data
+
+VLM_MRDB = mariadb
+VLM_WRDP = wordpress
+
+STP_FILE = setup.sh
+DCK_CMP = docker-compose.yml
+
+DOCKER_COMPOSE = docker compose -f $(SRC_PATH)/$(DCK_CMP)
+VOLUMES = $(VLM_PATH)/$(VLM_WRDP) $(VLM_PATH)/$(VLM_MRDB)
+SETUP_SCRIPT = $(SRC_PATH)/$(STP_FILE)
 
 # **************************************************************************** #
 #                                   RULES                                      #
@@ -22,29 +31,29 @@ SETUP_SCRIPT = ./srcs/setup.sh
 all: setup up
 
 setup:
-	@echo "[+] Checking environment..."
-	@bash $(SETUP_SCRIPT)
+	echo "[+] Checking environment..."
+	bash $(SETUP_SCRIPT)
 
 up:
-	@echo "$(GREEN)[+] Starting containers...$(NC)"
-	@$(DOCKER_COMPOSE) up -d --build
+	echo "[+] Starting containers...$(NC)"
+	$(DOCKER_COMPOSE) up -d --build
 
 build:
-	@echo "$(GREEN)[+] Building images...$(NC)"
-	@$(DOCKER_COMPOSE) build
+	echo "[+] Building images...$(NC)"
+	$(DOCKER_COMPOSE) build
 
 down:
-	@echo "$(YELLOW)[+] Stopping and removing containers...$(NC)"
-	@$(DOCKER_COMPOSE) down
+	echo "[+] Stopping and removing containers...$(NC)"
+	$(DOCKER_COMPOSE) down
 
 clean: down
-	@echo "$(YELLOW)[+] Removing orphan containers...$(NC)"
-	@docker system prune -f
+	echo "[+] Removing orphan containers...$(NC)"
+	docker system prune -f
 
 fclean: clean
-	@echo "$(RED)[+] Removing all volumes and images related to the project...$(NC)"
-	@$(DOCKER_COMPOSE) down --volumes --rmi all
-	@docker volume rm $(VOLUMES) 2>/dev/null || true
+	echo "[+] Removing all volumes and images related to the project...$(NC)"
+	$(DOCKER_COMPOSE) down --volumes --rmi all
+	docker volume rm $(VOLUMES) 2>/dev/null || true
 
 re: fclean all
 
--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PATH="./srcs"
+SRC_PATH="./srcs"
 
 #~~~~~~~~~~~~~~~~~DEPENDANCIES INSTALLATION~~~~~~~~~~~~~~~~~
 #Check dependancies installed
 for cmd in openssl docker "docker compose"; do
     if ! command -v $cmd >/dev/null 2>&1; then
+        echo "Testing: '$cmd'"
         #Check sudo mode
         if [ "$EUID" -ne 0 ]; then
             echo "Error : $cmd not installed. Restart script as sudo"
@@ -13,7 +14,7 @@ for cmd in openssl docker "docker compose"; do
         else
             echo "Installing $cmd..."
             if [[ "$cmd" == "docker" || "$cmd" == "docker-compose" ]]; then
-            	sudo $(PATH)/docker_install.sh 
+            	sudo ${SRC_PATH}/docker_install.sh 
             else
             	apt-get update && apt-get install -y $cmd
 	    fi
@@ -25,31 +26,31 @@ done
 #~~~~~~~~~~~~~~~~~CREATION .ENV~~~~~~~~~~~~~~~~~
 #Var declaration
 echo "~~~~~.env~~~~~"
-if ! [ -f $(PATH)/.env ]; then
-    MYSQL_DB_NAME=inception_base;
-    MYSQL_USER_NAME=sylabbe;
-    MYSQL_USER_PASSWORD=sylabbe1669; #$(openssl rand -hex 16);
-    MYSQL_ROOT_PASSWORD=root;
+if  [ ! -f ${SRC_PATH}/.env ]; then
+    MYSQL_DB_NAME=inception_base
+    MYSQL_USER_NAME=sylabbe
+    MYSQL_USER_PASSWORD=sylabbe1669 #$(openssl rand -hex 16);
+    MYSQL_ROOT_PASSWORD=root
 
-    DOMAIN_NAME=sylabbe.42.fr;
-    MARIADB_PORT=3306;
+    DOMAIN_NAME=sylabbe.42.fr
+    MARIADB_PORT=3306
 
-    SQL_SERVICE=mariadb;
-    PHP_SERVICE=wordpress;
-    SRV_SERVICE=nginx;
+    SQL_SERVICE=mariadb
+    PHP_SERVICE=wordpress
+    SRV_SERVICE=nginx
     #create .env
-    cp $(PATH)/.env.example $(PATH)/.env
+    cp ${SRC_PATH}/.env.example ${SRC_PATH}/.env
     echo "Copy .env.example to .env"
 
-    sed -i "s/^MYSQL_DB_NAME=.*/MYSQL_DB_NAME=${MYSQL_DB_NAME}/" $(PATH)/.env
-    sed -i "s/^MYSQL_USER_NAME=.*/MYSQL_USER_NAME=${MYSQL_USER_NAME}/" $(PATH)/.env
-    sed -i "s/^MYSQL_USER_PASSWORD=.*/MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD}/" $(PATH)/.env
-    sed -i "s/^DOMAIN_NAME=.*/DOMAIN_NAME=${DOMAIN_NAME}/" $(PATH)/.env
-    sed -i "s/^MARIADB_PORT=.*/MARIADB_PORT=${MARIADB_PORT}/" $(PATH)/.env
-    sed -i "s/^SQL_SERVICE=.*/SQL_SERVICE=${SQL_SERVICE}/" $(PATH)/.env
-    sed -i "s/^PHP_SERVICE=.*/PHP_SERVICE=${PHP_SERVICE}/" $(PATH)/.env
-    sed -i "s/^SRV_SERVICE=.*/SRV_SERVICE=${SRV_SERVICE}/" $(PATH)/.env
-    sed -i "s/^MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/" $(PATH)/.env
+    sed -i "s/^MYSQL_DB_NAME=.*/MYSQL_DB_NAME=${MYSQL_DB_NAME}/" ${SRC_PATH}/.env
+    sed -i "s/^MYSQL_USER_NAME=.*/MYSQL_USER_NAME=${MYSQL_USER_NAME}/" ${SRC_PATH}/.env
+    sed -i "s/^MYSQL_USER_PASSWORD=.*/MYSQL_USER_PASSWORD=${MYSQL_USER_PASSWORD}/" ${SRC_PATH}/.env
+    sed -i "s/^DOMAIN_NAME=.*/DOMAIN_NAME=${DOMAIN_NAME}/" ${SRC_PATH}/.env
+    sed -i "s/^MARIADB_PORT=.*/MARIADB_PORT=${MARIADB_PORT}/" ${SRC_PATH}/.env
+    sed -i "s/^SQL_SERVICE=.*/SQL_SERVICE=${SQL_SERVICE}/" ${SRC_PATH}/.env
+    sed -i "s/^PHP_SERVICE=.*/PHP_SERVICE=${PHP_SERVICE}/" ${SRC_PATH}/.env
+    sed -i "s/^SRV_SERVICE=.*/SRV_SERVICE=${SRV_SERVICE}/" ${SRC_PATH}/.env
+    sed -i "s/^MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/" ${SRC_PATH}/.env
     echo "Variable substitution made"
     echo ".env created"
 else
@@ -58,7 +59,7 @@ fi
 #~~~~~~~~~~~~~~~~~CREATION SSL CERTIFICATES~~~~~~~~~~~~~~~~~
 #create directory for certs
 echo "~~~~~SSL certs~~~~~"
-if ! [-d ./nginx/certs]; then
+if  [ ! -d ./nginx/certs ]; then
     mkdir -p ./nginx/certs
     echo "./nginx/certs directory created"
     #generate SSL certs
